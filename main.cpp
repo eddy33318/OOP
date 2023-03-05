a70e0c00596592369241eadbd753e377a49dce57
eddy33318 <eduard.voicu@lervs.ro>
2023-03-05 12:52:29 +0200

    modificari in setter

--- main.cpp
+++ main.cpp
@@ -3,18 +3,25 @@
 using namespace std;
 #include "student.h"
 #include "global.h"
-char s1[101];
-char s2[101];
+char s1[11];
+char s2[11];
+char s3[11];
 int main()
 {
     student andrei;
     student edi;
+    student ion;
     strcpy(s1, "Andrei");
     strcpy(s2, "Edi");
+    strcpy(s3, "Ion");
     andrei.set_name(s1);
     edi.set_name(s2);
+    ion.set_name(s3);
     cout << edi.get_name() << "\n";
     cout << andrei.get_name() << "\n";
+    ion.set_nota_engleza(1.0);
+    ion.set_nota_istorie(11.2);
+    ion.set_nota_mate(0);
     andrei.set_nota_engleza(7.3);
     andrei.set_nota_istorie(8.5);
     andrei.set_nota_mate(9.7);
@@ -23,6 +30,7 @@ int main()
     edi.set_nota_mate(9.8);
     cout << edi.nota_AVG() <<"\n";
     cout << andrei.nota_AVG() <<"\n";
+    cout << ion.nota_AVG() << "\n";
     if(compAVG(edi, andrei) == 1)cout << "Edi are media mai mare\n";
     else if(compAVG(edi, andrei) == 0)cout << "Medii egale\n";
     else
--- student.cpp
+++ student.cpp
@@ -6,14 +6,17 @@ void student::set_name(const char *name)
  } 
 void student::set_nota_mate(float nota_mate)
 {
+    if(nota_mate < 1 || nota_mate > 10)return;
   this -> nota_mate = nota_mate;
 }
 void student::set_nota_istorie(float nota_istorie)
 {
+    if(nota_istorie < 1 || nota_istorie > 10)return;
     this -> nota_istorie = nota_istorie;
 }
 void student::set_nota_engleza(float nota_engleza)
 {
+    if(nota_engleza < 1 || nota_engleza > 10)return;
     this -> nota_engleza = nota_engleza;
 }
 char* student::get_name()
